c0583f3de9266c649b6c1fc39efbc1b2b34c302e
TZOOCK <eyal.tzoock@gmail.com>
2017-02-28 00:41:51 +0200

    add-list and add-card interactivnes

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
-/**
- * Created by אייל on 2/26/2017.
- */
+const wraper = document.getElementById('contflex');
+const btn = document.getElementById('addList');
+btn.addEventListener('click', addList);
+
+function addList() {
+  const newColumn = document.createElement('div');
+  newColumn.innerHTML = `
+    <header>List name</header>
+    <div class="main-column">
+      <ul class="cards-list">
+
+      </ul>
+    </div>
+    <footer><button class="add-card">Add a Card</button></footer>`;
+  newColumn.setAttribute('class', 'column');
+  wraper.insertBefore(newColumn, btn);
+  newCardBtn();
+}
+
+
+
+function newCardBtn() {
+  let addCardBtn = document.querySelectorAll(".add-card");
+  for (let i = 0; i < addCardBtn.length; i++) {
+    addCardBtn[i].addEventListener("click", addCard)
+  }
+}
+newCardBtn();
+
+// addCardBtn[i].addEventListener("click", addCard(i));
+
+function addCard() {
+
+  const newLi = document.createElement('li');
+  newLi.textContent = 'new card';
+  const ul = event.target.parentNode.previousSibling.previousSibling;
+const cardsList = ul.querySelector('.cards-list');
+  console.log(cardsList);
+  cardsList.appendChild(newLi);
+}
+
+
+
+
